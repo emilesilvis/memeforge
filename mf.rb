3d6800b7e2b64c39183e724b24573c78c50138c3
Emile Silvis <emilesilvis@ubuntu-VirtualBox.(none)>
2012-12-06 09:58:25 +0100

    Meme view

--- mf.rb
+++ mf.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'fileutils'
+require_relative "mxit.rb"
 
 
 get '/' do
@@ -7,11 +8,10 @@ get '/' do
 end
 
 post '/upload' do
-	FileUtils.move(params['file'][:tempfile].path,'uploads/')
+	@mxit = Mxit.new(request.env)
+	@filename = Time.now.to_s
+	FileUtils.mkdir_p('uploads/' + @mxit.user_id)
+	FileUtils.move(params['file'][:tempfile].path,'uploads/' + @mxit.user_id + '/' + @filename)
+	@path = 'uploads/' + @mxit.user_id + '/' + @filename
 	erb :meme
-end
-
-
-post '/meme' do
-	"Your top text is " + params['top'] + ", and your bottom text is " + params['bottom']
 end
