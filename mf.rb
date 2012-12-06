17354ffdf4145f70f6983e47352f09ead2daef0e
Emile Silvis <emilesilvis@ubuntu-VirtualBox.(none)>
2012-12-06 13:55:28 +0200

    Split the forms

--- mf.rb
+++ mf.rb
@@ -2,17 +2,27 @@ require 'sinatra'
 require 'fileutils'
 require_relative "mxit.rb"
 
+enable :sessions
+
 get '/' do
-	erb :index
+	erb :image
 end
 
-post '/upload' do
+post '/image' do
 	@mxit = Mxit.new(request.env)
 	@filename = Time.now.year.to_s + '-' + Time.now.month.to_s + '-' + Time.now.day.to_s + '-' + Time.now.hour.to_s + '-' + Time.now.min.to_s + '-' + Time.now.sec.to_s + '.jpg' #not yet unique	
 	FileUtils.mkdir_p('public/uploads/' + @mxit.user_id)
 	FileUtils.move(params['file'][:tempfile].path,'public/uploads/' + @mxit.user_id + '/' + @filename, :force => true)
-	@path = 'uploads/' + @mxit.user_id + '/' + @filename
-	@top = params['top']
-	@bottom = params['bottom']
+	session[:path] = 'uploads/' + @mxit.user_id + '/' + @filename
+	erb :top
+end
+
+post '/top' do
+	session[:top] = params['top']
+	erb :bottom
+end
+
+post '/bottom' do
+	session[:bottom] = params['bottom']
 	erb :meme
 end
