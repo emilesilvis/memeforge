2ee13032f11a4bcf7b80c6b308045332242ee2da
Emile Silvis <emilesilvis@ubuntu-VirtualBox.(none)>
2012-12-06 19:26:20 +0200

    Fixed filename extensions and formatting

--- mf.rb
+++ mf.rb
@@ -10,11 +10,12 @@ end
 
 post '/image' do
 	@mxit = Mxit.new(request.env)
-	@filename = Time.now.year.to_s + '-' + Time.now.month.to_s + '-' + Time.now.day.to_s + '-' + Time.now.hour.to_s + '-' + Time.now.min.to_s + '-' + Time.now.sec.to_s + '.jpg' #not yet unique	
+	@filename = Time.now.year.to_s + '-' + Time.now.month.to_s + '-' + Time.now.day.to_s + '-' + Time.now.hour.to_s + '-' + Time.now.min.to_s + '-' + Time.now.sec.to_s + '-' + params['file'][:filename]	
 	FileUtils.mkdir_p('public/uploads/' + @mxit.user_id)
 	FileUtils.move(params['file'][:tempfile].path,'public/uploads/' + @mxit.user_id + '/' + @filename, :force => true)
 	session[:path] = 'uploads/' + @mxit.user_id + '/' + @filename
 	erb :top
+
 end
 
 post '/top' do
@@ -28,5 +29,5 @@ post '/bottom' do
 end
 
 error do
-  'Sorry there was a nasty error - ' + env['sinatra.error'].name
+  	''+ env['sinatra.error'].name
 end
