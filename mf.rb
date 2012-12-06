e5c06cee9fe09617a218a3406f3222c67ed8348d
Emile Silvis <emilesilvis@ubuntu-VirtualBox.(none)>
2012-12-06 10:59:29 +0100

    Fixed paths

--- mf.rb
+++ mf.rb
@@ -9,9 +9,9 @@ end
 
 post '/upload' do
 	@mxit = Mxit.new(request.env)
-	@filename = Time.now.to_s
+	@filename = Time.now.hour.to_s + '.jpg'
 	FileUtils.mkdir_p('public/uploads/' + @mxit.user_id)
 	FileUtils.move(params['file'][:tempfile].path,'public/uploads/' + @mxit.user_id + '/' + @filename)
-	@path = 'public/uploads/' + @mxit.user_id + '/' + @filename
+	@path = 'uploads/' + @mxit.user_id + '/' + @filename
 	erb :meme
 end
